fea07e1c7e521bf580a307a4b58ea93483aa2816
Trent Nelson <trentnelson97@gmail.com>
2025-11-24 20:24:35 -0700

    Created portfolio site

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+const myListOfItems = document.querySelectorAll("section");
+
+// a comma deliniated list of name/value pairs controlling how the observer works
+let observerOptions = {
+    //null is the default and references the viewport
+    root: null,
+    //alters the viewport. negative values decrease the size.
+    rootMargin: '0px 0px 0px 0px',
+    //0 is barely showing, 1 is fully showing
+    threshold: .2
+  }
+
+// AllItems is a list of all elements being watched
+const myObserver = new IntersectionObserver(allItems => {
+    allItems.forEach(singleItem => {
+        if (singleItem.isIntersecting){
+            hiliteNav(singleItem.target)
+        }
+    })
+}, observerOptions)
+
+// function to hilight the current navigation items
+function hiliteNav(x) {
+	document.querySelector('.active').classList.remove('active');
+	let theid = x.getAttribute('id');
+	let newActiveLink = document.querySelector(`[href="#${theid}"]`)
+	newActiveLink.parentElement.classList.add('active');
+}
+
+//call the function for each element in the list
+myListOfItems.forEach(item => {
+    myObserver.observe(item)
+});
+
+////////////////////////////
+const btnElement = document.querySelector("#hamburgerBtn");
+const navElement = document.querySelector("#primaryNav");
+
+btnElement.addEventListener("click", () => {
+    btnElement.classList.toggle("open");
+    navElement.classList.toggle("open");
+});
